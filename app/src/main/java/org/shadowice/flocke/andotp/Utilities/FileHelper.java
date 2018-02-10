f7bc8bd0563916ea8b0865d75a384713580cbfa0
RichyHBM <gitemail@richyhbm.co.uk>
2018-02-10 17:26:59 +0000

    Move backupFileName into static class

--- app/src/main/java/org/shadowice/flocke/andotp/Utilities/FileHelper.java
+++ app/src/main/java/org/shadowice/flocke/andotp/Utilities/FileHelper.java
@@ -121,5 +121,31 @@ public class FileHelper {
             out.close();
         }
     }
+
+    public static String backupFilename(Context context, Constants.BackupType type) {
+        Settings settings = new Settings(context);
+        switch (type) {
+            case PLAIN_TEXT:
+                if (settings.getIsAppendingDateTimeToBackups()) {
+                    return String.format(Constants.BACKUP_FILENAME_PLAIN_FORMAT, Tools.getDateTimeString());
+                } else {
+                    return Constants.BACKUP_FILENAME_PLAIN;
+                }
+            case ENCRYPTED:
+                if (settings.getIsAppendingDateTimeToBackups()) {
+                    return String.format(Constants.BACKUP_FILENAME_CRYPT_FORMAT, Tools.getDateTimeString());
+                } else {
+                    return Constants.BACKUP_FILENAME_CRYPT;
+                }
+            case OPEN_PGP:
+                if (settings.getIsAppendingDateTimeToBackups()) {
+                    return String.format(Constants.BACKUP_FILENAME_PGP_FORMAT, Tools.getDateTimeString());
+                } else {
+                    return Constants.BACKUP_FILENAME_PGP;
+                }
+        }
+
+        return Constants.BACKUP_FILENAME_PLAIN;
+    }
 }
 
