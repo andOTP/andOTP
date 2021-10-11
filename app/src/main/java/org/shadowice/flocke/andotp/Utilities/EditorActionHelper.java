61ed5299242d9b32e04300588fe37cf0a2952e12
wtimme <wtimme@users.noreply.github.com>
2021-10-11 14:25:08 +0200

    Ensure that `event` exists before accessing it (#866)
    When entering the password using the Magikeyboard from KeePassDX,
    the `event` was `null`, which resulted in the app crashing.

--- app/src/main/java/org/shadowice/flocke/andotp/Utilities/EditorActionHelper.java
+++ app/src/main/java/org/shadowice/flocke/andotp/Utilities/EditorActionHelper.java
@@ -13,7 +13,10 @@ public class EditorActionHelper {
     private EditorActionHelper() { /* not allowed */ }
 
     public static boolean isActionDoneOrKeyboardEnter(int actionId, KeyEvent event) {
-        boolean isKeyboardEnterEvent = event.getAction() == ACTION_DOWN && (event.getKeyCode() == KEYCODE_ENTER || event.getKeyCode() == KEYCODE_NUMPAD_ENTER);
+        boolean isKeyboardEnterEvent = false;
+        if (event != null) {
+            isKeyboardEnterEvent = event.getAction() == ACTION_DOWN && (event.getKeyCode() == KEYCODE_ENTER || event.getKeyCode() == KEYCODE_NUMPAD_ENTER);
+        }
 
         return actionId == EditorInfo.IME_ACTION_DONE || isKeyboardEnterEvent;
     }
